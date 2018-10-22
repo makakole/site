e1c98084cee856124c68b82e330059a67b5d5f72
Makakole <44157996+makakole@users.noreply.github.com>
2018-10-22 14:26:40 +0200

    Add files via upload

---
+++ personalwebsite/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	 $(".options").on("click", function(){
+
+	 	$(".slide").toggle( "slide" );
+
+	 });
+
+	 $("#close").on("click", function(){
+
+	 	$(".slide").toggle( "slide" );
+
+	 });
+
+});
